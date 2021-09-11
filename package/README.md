f398eb28e57a925cf60adc5b6848c71cd3ef0568
Xavier Cho <mysticfallband@gmail.com>
2021-09-11 11:29:33 +0900

    Preparations for publishing stub modules

---
+++ package/README.md
@@ -0,0 +1,32 @@
+Blender / UPBGE API Stubs
+=========================
+
+This module provides type information for [Blender](https://www.blender.org) and 
+[UPBGE](https://upbge.org)'s Python API, following the 
+[PEP-561](https://www.python.org/dev/peps/pep-0561/) standard.
+
+It was generated using [BPY Stub Generator](https://github.com/mysticfall/bpystubgen) which 
+is also available on [PyPI](https://pypi.org/project/bpystubgen) as a separate module.
+
+## Installation ##
+
+There are two variants of the API stubs, one for Blender and the other for UPBGE. For Blender, 
+you can install `blender-stubs` module with appropriate version qualifier as follows: 
+
+```bash
+$ pip install blender-stubs=2.93.*
+```
+This will install the latest module for Blender 2.93, and for now stubs are provided for Blender 
+2.80 and onwards. You can also specify the next unreleased version as `3.0.*` to get the latest 
+snapshot of the module.
+
+As for UPBGE, stubs are available for the upcoming 0.3 release, which you can install as follows:
+
+```bash
+$ pip install upbge-stubs=0.3.*
+```
+
+## License ##
+
+This project is provided under the terms of 
+[GNU General Public License v3 (GPL3)](https://github.com/mysticfall/bpystubgen/blob/main/LICENSE).
---
+++ package/setup.py
@@ -0,0 +1,46 @@
+import glob
+import os
+from datetime import datetime
+
+from setuptools import setup
+
+app_name = os.environ["APP_NAME"] if "APP_NAME" in os.environ else "Blender"
+app_version = os.environ["APP_VERSION"] if "APP_VERSION" in os.environ else "3.0.dev"
+module_version = os.environ["MODULE_VERSION"] if "MODULE_VERSION" in os.environ else "0.1.2"
+
+name = f"{app_name.lower()}-stubs"
+
+if app_version.endswith("dev"):
+    timestamp = datetime.today().strftime("%Y%m%d%H%M")
+    version = ".".join((app_version, module_version, timestamp))
+else:
+    version = ".".join((app_version, module_version))
+
+files = glob.glob("**/*.pyi", recursive=True)
+packages = set(map(lambda f: f.split(os.sep)[0].replace(".pyi", ""), files))
+package_data = {pkg: ["py.typed", "*.pyi", "**/*.pyi"] for pkg in packages}
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name=name,
+    version=version,
+    author="Xavier Cho",
+    author_email="mysticfallband@gmail.com",
+    description="API stubs for Blender and UPBGE generated with bpystubgen.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mysticfall/bpystubgen",
+    packages=packages,
+    package_data=package_data,
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Topic :: Multimedia :: Graphics :: 3D Modeling",
+        "Topic :: Multimedia :: Graphics :: 3D Rendering",
+        "Topic :: Text Editors :: Integrated Development Environments (IDE)"
+    ]
+)
