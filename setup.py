d7ba90e96e940095f60fe84e8ea76ac1703755a5
Xavier Cho <mysticfallband@gmail.com>
2021-09-13 09:39:49 +0900

    Release 0.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="bpystubgen",
-    version="0.1.4",
+    version="0.1.5",
     author="Xavier Cho",
     author_email="mysticfallband@gmail.com",
     description="A utility to generate Python API stubs from documentation files in reStructuredText format.",
